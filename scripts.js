492964ba76a13b3170dc60df7dda79e4d6b9fb9c
victorbruno8 <euvbruno8@gmail.com>
2024-10-09 10:48:28 -0300

    Previsão do Tempo - Responsivo

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const key = '2674e364e56ec7c70b82de1b113acca8'
+
+
+function colocarDadosNaTela(dados) {
+    document.querySelector('.time-city').innerHTML ="Tempo em " + dados.name
+    document.querySelector('.temperature').innerHTML = Math.floor(dados.main.temp) + '°C'
+    document.querySelector('.climate').innerHTML = dados.weather[0].description
+    document.querySelector('.moisture').innerHTML ="Umidade: " + dados.main.humidity + "%"
+    document.querySelector('img').src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
+
+}
+
+
+
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( Resposta => Resposta.json())
+
+    colocarDadosNaTela(dados)
+
+}
+
+
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector('.input-city').value
+
+    buscarCidade(cidade)
+    
+}
